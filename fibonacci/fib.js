7af5ec38282ed2a22c84d79a5dcac4fc313ddb0b
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-25 17:27:02 +0100

    implement fibonacci

---
+++ fibonacci/fib.js
@@ -0,0 +1,27 @@
+//ts-worksheet-with-variables
+function fib(n) {
+	if (n <= 0) return [];
+	if (n === 1) return [0];
+	if (n === 2) return [0, 1];
+	const prev = fib(n - 1);
+	return [...fib(n - 1), prev[n - 2] + prev[n - 3]];
+}
+
+function fibHateYourColleaguesEdition() {
+	return n <= 0
+		? []
+		: n === 1
+			? [0]
+			: n === 2
+				? [0, 1]
+				: [...fib(n - 1), fib(n - 1)[n - 2] + fib(n - 1)[n - 3]];
+}
+
+function fibRec(n) {
+	return n === 0 ? [] : n === 1 ? [0] : [0, 1];
+	const ans = [0, 1];
+	for (let i = 2; i <= n; i++) {
+		ans.push(ans[i - 2] + ans[i - 1]);
+	}
+	return ans;
+}
